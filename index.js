bae41d19655d9d662e5aed3e7eda954789afa77f
Urban Sandén <hej@urre.me>
2020-04-28 21:45:37 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,68 @@
+var factor = 0.001;
+var minDelta = 0.5;
+var scrollSpeed = 10;
+var scrollTimer;
+var restartTimer;
+var isScrolling = false;
+var prevPos = 0;
+var currentPos = 0;
+var currentTime;
+var prevTime;
+var timeDiff;
+
+/* You can scroll manually to break auto scroll */
+handleScroll = () => {
+  currentPos = window.scrollY;
+  clearInterval(scrollTimer);
+  if (restartTimer) {
+    clearTimeout(restartTimer);
+  }
+};
+
+/* Auto scroll */
+autoScroll = newValue => {
+    window.scrollTo(0, 0);
+  if (newValue) scrollSpeed = factor * newValue;
+
+  if (scrollTimer) clearInterval(scrollTimer);
+
+  scrollTimer = setInterval(() => {
+    currentTime = Date.now();
+    if (prevTime && !isScrolling) {
+      // if (!isScrolling) {
+        timeDiff = currentTime - prevTime;
+        currentPos += scrollSpeed * timeDiff;
+        if (Math.abs(currentPos - prevPos) >= minDelta) {
+          isScrolling = true;
+          window.scrollTo(0, currentPos);
+          isScrolling = false;
+          prevPos = currentPos;
+          prevTime = currentTime;
+        }
+      // }
+    } else {
+      prevTime = currentTime;
+    }
+  }, 1000 / 120);
+};
+
+window.addEventListener("scroll", e => {
+  currentPos = window.scrollY;
+});
+
+/* Listen for scroll */
+window.addEventListener("wheel", handleScroll);
+
+/* Escape key stops autoscroll */
+document.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+    clearInterval(scrollTimer);
+  }
+})
+
+/* Click to stop autoscroll */
+document.addEventListener('click', (e) => {
+  clearInterval(scrollTimer);
+})
+
+autoScroll(200);
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Scroll Showoff
+
+Click the browser button and lean back while the page scrolls down slowly. Perfect for a screen recording
+
+## Install
+
++ Clone repo or download zip
++ Open upp Google Chrome Extensions. Window → Extensions. Or goto `chrome://extensions/`
++ Click Developer Mode
++ Click Load Unpacked Extension
++ Navigate to the folder where you saved it
+
+## Usage
++ Click the extension browser button icon to get branch name in your clipboard
+
+### Changelog
++ 2020-04-26: 🎉 First version released
+
+### License
+
+MIT © [Urban Sanden](https://twitter.com/urre)
